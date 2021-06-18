9d4c457a6305667e43cfc1a3f17c9585b7837c50
FishHuahua <86067517+FishHuahua@users.noreply.github.com>
2021-06-18 23:58:59 +0800

    修改test1.py 增加了函数get_system_info

--- test1.py
+++ test1.py
@@ -1,5 +1,10 @@
+import sys
 def print_hello_world():
   print("hello world")
-  
+
+def get_system_info():
+   print(sys.platform)
+    
 if __name__ == "__main__":
   print_hello_word()
+  get_system()
